282776cc7bfe7b93c24a7f1b0098db53bea2dc8f
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2023-11-22 21:01:38 +0100

    wall building

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ const MAX_BRICKS = 10_000;
 if (amountOfBricks > MAX_BRICKS) {
   return "Naah, too much...here's my resignation.";
 }
+
+const halfBrick = "■";
+const fullBrick = halfBrick.repeat(2);
+const mortar = "|";
+let wall = "";
+
+for (let row= 0; row < rows; row++) {
+  const isRowEven = row % 2 === 0;
+  if (isRowEven) {
+    wall += (fullBrick + mortar).repeat(columns - 1) + fullBrick + "\n";
+  } else {
+    wall += halfBrick + (mortar + fullBrick).repeat(columns - 1) + mortar + halfBrick + "\n";   
+  }
+}
+
+console.log(wall);
+return wall;
