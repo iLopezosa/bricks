9aeef0e56e1a710851f5b3d47b3ec4ba8c63ee77
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2023-11-22 20:52:41 +0100

    parsing args

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ if (argv.length > 4) {
   console.error("Incorrect amount of arguments");
   return null;
 }
+
+const rows = parseInt(argv[2]);
+const columns = parseInt(argv[3]);
+if (
+  isNaN(rows)
+  || isNaN(columns)
+  || rows < 1
+  || columns < 1
+) {
+  console.error("Incorrect arguments");
+  return null;
+}
